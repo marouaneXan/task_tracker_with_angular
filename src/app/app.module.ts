fab4f8068a4584f3f03fe33763d496a28895568a
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-24 20:42:15 +0100

    generate a modalAddTask component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
+import { ModalAddTaskComponent } from './components/modal-add-task/modal-add-task.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    ModalAddTaskComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/modal-add-task/modal-add-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-add-task',
+  templateUrl: './modal-add-task.component.html',
+  styleUrls: ['./modal-add-task.component.css']
+})
+export class ModalAddTaskComponent {
+
+}
