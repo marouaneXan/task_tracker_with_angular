a64c56971b947e1970ef814b86668625defe7dc7
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-24 11:43:02 +0100

    generate a header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent
+    ServersComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
