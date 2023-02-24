9dc8df09f4521ee6889803b46ed913264205c472
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-24 13:02:15 +0100

    generate tasks-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ServersComponent } from './servers/servers.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { TasksComponent } from './components/tasks/tasks.component';
     ServersComponent,
     HeaderComponent,
     ButtonComponent,
-    TasksComponent
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
