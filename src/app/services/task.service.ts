96f9f0dac519ad68a94fe034a72fb36a0b1178cd
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-24 15:38:54 +0100

    get tasks using HttpClient

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import {TASKS} from "../mock-tasks"
 import { Task } from '../Task';
-import {Observable,of} from 'rxjs'
+import {Observable} from 'rxjs'
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl="http://localhost:3000/tasks"
 
-  constructor() { 
+  constructor(private http:HttpClient) { 
   }
 getTasks():Observable<Task[]>{
-    const tasks= of(TASKS)
-    return tasks
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
